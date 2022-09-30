30a7e91d82e81b1fb9a6ae6918f8ea2830ced065
HaydenAtH <114445268+HaydenAtH@users.noreply.github.com>
2022-09-30 15:03:27 -0500

    Added src in main repo

---
+++ OutputService/StoryOutput.java
@@ -0,0 +1,60 @@
+package OutputService;
+import Renderer.AsciiRenderer;
+import Renderer.Image;
+import StoryService.StoryBranch;
+import StoryService.StoryNode;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class StoryOutput {
+
+
+    protected String name;
+    protected String textOutput;
+    protected Image img;
+
+    protected StoryNode parentNode;
+
+
+
+    public StoryOutput(String name, String textOutput, Image img){
+        this.name = name;
+        this.textOutput = textOutput;
+        this.img = img;
+    }
+
+
+    //TODO Add exception for no image (Allow no image to be shown)
+    public void activate() throws IOException {
+        System.out.println("Placeholder");
+        AsciiRenderer.render(img);
+    }
+
+
+
+    public void setImg(Image img){
+        this.img = img;
+    }
+
+    public void setParentNode(StoryNode parentNode){
+        this.parentNode = parentNode;
+    }
+
+    // Query Functions
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getTextOutput() {
+        return this.textOutput;
+    }
+
+    public Image getImg() {
+        return this.img;
+    }
+
+
+
+}
