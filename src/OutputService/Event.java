7be11a1b3d245b79f8ff47c3b8853408f81c3a97
HaydenAtH <114445268+HaydenAtH@users.noreply.github.com>
2022-09-30 15:02:51 -0500

    Restructuring

--- src/OutputService/Event.java
+++
@@ -1,9 +0,0 @@
-package OutputService;
-
-import Renderer.Image;
-
-public class Event extends StoryOutput{
-    public Event(String name, String textOutput, Image img) {
-        super(name, textOutput, img);
-    }
-}
